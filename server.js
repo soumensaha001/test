53f2fb63a2804cf9ee41628a97217439eab17a3d
Soumen Saha <45451838+soumensaha001@users.noreply.github.com>
2021-06-20 10:04:06 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+ 
+
+server.listen(server_port, server_ip_address, function () {
+
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+
+});
