c46696d9df97830d2dba34f8ac9e6f31ae54ab38
Soumen Saha <45451838+soumensaha001@users.noreply.github.com>
2021-06-20 11:02:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,7 @@
-'use strict';
+var http = require('http');
 
-const express = require('express');
-
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
-
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('Hello World');
 });
-
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-
- 
-
-app.listen(server_port, server_ip_address, function () {
-
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
-
-});
+server.listen(8080);
